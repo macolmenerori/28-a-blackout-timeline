4f47f2605a9645dc27938c11e9ee84ee89a11ea3
macolmenerori <miguelangelcolmenero@gmail.com>
2025-11-13 11:34:21 +0100

    feature: Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
+import { Header } from './components/Header/Header';
 import { AppProviders } from './providers/AppProviders';
 
 export function App() {
   return (
     <AppProviders>
       <div className="App">
-        <header className="App-header">
-          <h1>28-A Blackout Timeline</h1>
-          <p>Full timeline of the blackout that affected Spain and Portugal on April 28, 2025.</p>
-        </header>
+        <Header />
       </div>
     </AppProviders>
   );
