97b7caa019a3390e9a5b2206f0c51140104a5627
macolmenerori <miguelangelcolmenero@gmail.com>
2026-04-09 11:36:21 +0200

    chore: Upgrade typescript to v6

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -16,3 +16,5 @@ declare module '*.module.sass' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+declare module '@macolmenerori/component-library/theme-switch-css' {}
